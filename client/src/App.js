17ae923576bb83fd06afa0c2d0e2a3fc9ae2af34
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-12-06 09:17:09 +0000

    class component axios request working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log(res);
+        this.setState({ users: res.data });
+      })
+      .catch(error => {
+        console.log("data not returned", error);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.users
+          ? this.state.users.map(user => {
+              return <Card users={user} />;
+            })
+          : null}
+      </div>
+    );
+  }
 }
-
 export default App;
